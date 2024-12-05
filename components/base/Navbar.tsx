bbde1d11d0416f87771dbddecdc01146f757bc9b
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-12-05 22:34:29 +0530

    icon chnaged

--- components/base/Navbar.tsx
+++ components/base/Navbar.tsx
@@ -1,10 +1,17 @@
+import Image from "next/image";
 import ViewCounter from "../ViewCounter";
 import ThemeSwitcher from "./ThemeSwitcher";
 import { Activity } from "lucide-react";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center border p-4 mb-4">
-      <Activity className="h-6 w-6" />
+      <Image
+        src="/Me.png"
+        alt="Me"
+        width={50}
+        height={50}
+        className="rounded-md"
+      />
 
       <div className="flex items-center gap-2">
         <ViewCounter />
